e2b589c9d5fa60857719fec570f0999222acc881
Adrian Trajano <45612732+AdrianTr97@users.noreply.github.com>
2025-01-23 21:57:51 -0300

    adicionando algumas funções de controle

--- Tarefa1.c
+++ Tarefa1.c
@@ -19,3 +19,19 @@
 
 // Definição do buzzer
 #define buzzer 21
+
+void config_gpio() {
+    // Incializando e configurando GPIOs como saidas
+    gpio_init(led_pin_green); gpio_set_dir(led_pin_green, GPIO_OUT);
+    gpio_init(led_pin_blue); gpio_set_dir(led_pin_blue, GPIO_OUT);
+    gpio_init(led_pin_red); gpio_set_dir(led_pin_red, GPIO_OUT);
+    gpio_init(buzzer); gpio_set_dir(buzzer, GPIO_OUT);
+}
+// Função para acionar os pinos
+void control_gpio(uint8_t red, uint8_t blue, uint8_t green, uint8_t buz) {
+    gpio_put(led_pin_red, red);
+    gpio_put(led_pin_blue, blue);
+    gpio_put(led_pin_green, green);
+    gpio_put(buzzer, buz);
+}
+
