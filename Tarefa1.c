72c57b9affdd6682d4299444ff66f30f004ad7a5
Adrian Trajano <45612732+AdrianTr97@users.noreply.github.com>
2025-01-23 21:56:42 -0300

    incluindo bibliotecas e GPIOs iniciais

--- Tarefa1.c
+++ Tarefa1.c
@@ -1,1 +1,21 @@
+#include <pico/stdlib.h>
+#include <stdio.h>
 
+// Definindo linhas e colunas do teclado matricial
+// Definindo linhas e colunas do teclado matricial
+#define linha1 8  
+#define linha2 7  
+#define linha3 6  
+#define linha4 5  
+#define coluna1 4 
+#define coluna2 3  
+#define coluna3 2  
+#define coluna4 28  
+
+// Definições dos LEDs RGB
+#define led_pin_green 11
+#define led_pin_blue 12
+#define led_pin_red 13
+
+// Definição do buzzer
+#define buzzer 21
